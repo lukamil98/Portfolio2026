c4525c3939174f243a271531b3c7555febd521fe
Luka <lmilanovic86@gmail.com>
2026-03-16 13:45:47 +0100

    Initial commit of 2026 portfolio

---
+++ script.js
@@ -0,0 +1,74 @@
+const form = document.getElementById("contactForm");
+const message = document.getElementById("form-message");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+const name = document.getElementById("name").value.trim();
+const email = document.getElementById("email").value.trim();
+const text = document.getElementById("message").value.trim();
+
+if(name.length < 3){
+message.textContent = "Name must be at least 3 characters";
+message.style.color = "red";
+return;
+}
+
+if(!email.includes("@")){
+message.textContent = "Please enter a valid email";
+message.style.color = "red";
+return;
+}
+
+if(text.length < 10){
+message.textContent = "Message must be at least 10 characters";
+message.style.color = "red";
+return;
+}
+
+message.textContent = "Message sent successfully!";
+message.style.color = "green";
+
+form.reset();
+
+});
+
+const weatherInfo = document.getElementById("weather-info");
+
+const apiKey = "669362dc1a46162b3966341d3c8201f4";
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&appid=${apiKey}`)
+.then(response => response.json())
+.then(data => {
+
+const temp = data.main.temp;
+const city = data.name;
+const description = data.weather[0].description;
+
+weatherInfo.textContent = `${city}: ${temp}°C, ${description}`;
+
+})
+.catch(() => {
+
+weatherInfo.textContent = "Weather data could not be loaded";
+
+});
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+// Toggle menu i hamburger animaciju
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+// Zatvaranje menija kada klikneš link
+const links = document.querySelectorAll("#nav-links a");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  });
+});
